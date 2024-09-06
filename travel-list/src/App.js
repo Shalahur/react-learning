bd1d3d457c89cfadb6fa22ab9baef0f8810b66cc
Shalahur <srpolash1991@gmail.com>
2024-09-06 23:12:42 +0600

    form submit event handler

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,3 +1,8 @@
+const initialItems = [
+	{id: 1, description: "Passports", quantity: 2, packed: false},
+	{id: 2, description: "Socks", quantity: 12, packed: true},
+];
+
 export default function App() {
 	return (
 		<div className="app">
@@ -14,18 +19,44 @@ function Logo() {
 }
 
 function Form() {
+
+	function handleSubmit(e) {
+		e.preventDefault();
+
+	}
+
 	return (
-		<div className="add-form">
+		<form className="add-form" onSubmit={handleSubmit}>
 			<h3> What do you need for your 😍 trip?</h3>
-		</div>
+			<select>
+				{Array.from({length: 20}).map((_, i) => (<option key={i} value={i}>{i + 1}</option>))}
+			</select>
+			<input type="text" placeholder="item..."/>
+			<button>Add</button>
+		</form>
 	);
 }
 
 function PackingList() {
 	return (
 		<div className="list">
-			List
+			<ul>
+				{initialItems.map((item) => (
+					<Item item={item} key={item.id}/>
+				))}
+			</ul>
 		</div>
+	);
+}
+
+function Item({item}) {
+	return (
+		<li>
+			<span style={item.packed ? {textDecoration: "line-through"} : {}}>
+				{item.quantity} {item.description}
+			</span>
+			<button>❌</button>
+		</li>
 	);
 }
 
