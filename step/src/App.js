e144f413dd253650d30e4a9efdc3a50a8cbc7bc5
Shalahur <srpolash1991@gmail.com>
2024-09-07 11:29:22 +0600

    introduce range input

--- step/src/App.js
+++ step/src/App.js
@@ -4,7 +4,8 @@ const messages = ["Learn React ⚛️", "Apply for jobs 💼", "Invest your new 
 
 export default function App() {
 
-	return (<div>
+	return (
+		<div>
 			<Steps/>
 			<Steps/>
 		</div>)
@@ -27,22 +28,22 @@ function Steps() {
 	}
 
 	return (<>
-			<button className="close" onClick={() => setIsOpen((o) => !o)}>&times;</button>
-			{isOpen && (<div className="steps">
-					<div className="numbers">
-						<div className={step >= 1 ? "active" : ""}>1</div>
-						<div className={step >= 2 ? "active" : ""}>2</div>
-						<div className={step >= 3 ? "active" : ""}>3</div>
-					</div>
+		<button className="close" onClick={() => setIsOpen((o) => !o)}>&times;</button>
+		{isOpen && (<div className="steps">
+			<div className="numbers">
+				<div className={step >= 1 ? "active" : ""}>1</div>
+				<div className={step >= 2 ? "active" : ""}>2</div>
+				<div className={step >= 3 ? "active" : ""}>3</div>
+			</div>
 
-					<p className="message">Step {step}: {messages[step - 1]}</p>
+			<p className="message">Step {step}: {messages[step - 1]}</p>
 
-					<div className="buttons">
-						<button style={{backgroundColor: "#7950f2", color: "#fff"}} onClick={handlePrevious}>Previous
-						</button>
-						<button style={{backgroundColor: "#7950f2", color: "#fff"}} onClick={handleNext}>Next</button>
+			<div className="buttons">
+				<button style={{backgroundColor: "#7950f2", color: "#fff"}} onClick={handlePrevious}>Previous
+				</button>
+				<button style={{backgroundColor: "#7950f2", color: "#fff"}} onClick={handleNext}>Next</button>
 
-					</div>
-				</div>)}
-		</>)
+			</div>
+		</div>)}
+	</>)
 }
--- step/src/index.js
+++ step/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Counter from "./Counter";
+import CounterV2 from "./CounterV2";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<App/>
-		<Counter/>
+		{/*<App/>*/}
+		{/*<Counter/>*/}
+		<CounterV2/>
 	</React.StrictMode>
 );
 
